593653e9c7c0511c98cb785cf26f4fc43fbf8815
Jose Aguilar <jose.aguilar.developer@gmail.com>
2024-01-26 14:45:28 -0400

    Feture: create the auth module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,12 +1,14 @@
+import { RouterOutlet } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 
+import { MaterialModule } from '../material/material.module';
+
 import { LayoutRootComponent } from './layouts/layout-root/layout-root.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
-import { RouterOutlet } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,6 @@ import { RouterOutlet } from '@angular/router';
     LoginPageComponent,
     RegisterPageComponent,
   ],
-  imports: [CommonModule, AuthRoutingModule, RouterOutlet],
+  imports: [AuthRoutingModule, CommonModule, MaterialModule, RouterOutlet],
 })
 export class AuthModule {}
